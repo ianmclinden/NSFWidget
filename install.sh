a688e94b6559aca405c58d36776daba8b79d1394
Ian McLinden <mclinden.ian@gmail.com>
2017-08-22 21:37:07 -0500

    Added scripts and files 4 th3 first tim3

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#  install.sh
+#  Install NSFWidget. Must be run as root.
+
+# Permission check
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+ONBOOT=1
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            echo "$.install.sh - NSFWidget installer"
+            echo " "
+            echo "$.install.sh [-h|--help] [--noboot]"
+            echo " "
+            echo "options:"
+            echo "-h, --help       show brief help"
+            echo "--noboot         specify an action to use"
+            exit 0
+            ;;
+        --noboot)
+            ONBOOT=0
+            shift
+            ;;
+        *)
+            echo "! Bad argument, ${1}"
+            echo "try $.install.sh --help"
+            exit 0
+            ;;
+    esac
+done
+
+#  Install python dependencies
+echo "- Installing python library dependencies"
+python -m pip install fourletterphat
+python -m pip install yahoo-finance
+
+#  Copy files && change permissions
+echo "- Copying files"
+
+printf "#!/bin/sh\ncd /etc/nsfw/\npython nsfw.py /etc/nsfw/stocks.conf" > /usr/local/bin/nsfw
+chmod a+x /usr/local/bin/nsfw
+echo "  * nsfw copied to /usr/local/bin/nsfw"
+
+mkdir -p /etc/nsfw/
+
+chmod a+x nsfw.py
+cp nsfw.py /etc/nsfw/
+echo "  * nsfw.py copied to /etc/nsfw/nsfw.py"
+
+chmod a+r stocks.conf
+cp stocks.conf /etc/nsfw/
+echo "  * stocks.conf copied to /etc/nsfw/stocks.conf"
+
+#  Add crontab job for startup - optional
+if [ "$ONBOOT" -eq "1" ]; then
+    crontab -l | { cat; echo "@reboot /usr/local/bin/nsfw > /var/log/nsfw.log 2>&1"; } | crontab -
+    echo "- Added crontab job at reboot"
+fi
---
+++ nsfw.py
@@ -0,0 +1,138 @@
+infotext="""
+    .##....##..########..########.##......##
+    .###...##.##..##..##.##.......##..##..##
+    .####..##.##..##.....##.......##..##..##
+    .##.##.##..########..######...##..##..##
+    .##..####.....##..##.##.......##..##..##
+    .##...###.##..##..##.##.......##..##..##
+    .##....##..########..##........###..###.
+
+    Nano Stocks 'Folio Widget
+
+    Lightweight stock ticker tape for the Raspberry Pi Zero W,
+    and Pimoroni's Four Letter pHAT. Configuration in stocks.conf, or
+    in config file specified as a command line argument.
+
+    Usage: `python nsfwidget.py [stocks_file.conf]`
+
+    Ian McLinden 2017
+    https://github.com/ianmclinden
+"""
+
+
+import sys
+try:
+    from collections import defaultdict
+    from functools import partial
+    import fourletterphat as flp
+    from yahoo_finance import Share
+    import time
+except Exception as e:
+    sys.exit( str(e)
+             +"\nTry installing the module with `pip install module-name`")
+
+
+def main():
+    print(infotext)
+    print("\nPress Ctrl-C to exit.\n")
+
+    flp.clear()
+    flp.print_str("NSFW")
+    flp.show()
+    flp.glow(period=1, duration=4)
+    flp.clear()
+    flp.show()
+
+    stocks = get_stocks_from_config("stocks.conf")
+
+    print("Watching Stocks:")
+    print("\n".join([sym+": "+share.get_name()+" ("+str(own)+" owned)" for sym,own,share in stocks]))
+
+    while(len(stocks)>0):
+        show_latest(stocks)
+
+    print("No stocks...")
+
+
+# Stock info logic
+def show_latest(stocks):
+    folio = defaultdict(float) # {'USD':'123.45'}, e.g.
+
+    for symbol,owned,share in stocks:
+        flp.clear()
+        flp.print_str(symbol)
+        flp.show()
+
+        pstr = symbol
+
+        try:
+            share.refresh()
+            time.sleep(0.5)
+
+            pstr +="  $"+ share.get_price() +" "+ share.get_currency()
+
+            if (owned>0):
+                # Append folio info if any stocks owned
+                total = float(share.get_price())*float(owned)
+                pstr +=", "+ str(round(owned,3))+ " OWNED, VAL $"+ str(round(total,2)) +" "+ share.get_currency()
+                folio[share.get_currency()] += total
+
+        except Exception as e:
+            pstr = "INET ERROR"
+
+        scroll_print_float_str(pstr, interval=0.2)
+        flp.clear()
+        flp.show()
+        time.sleep(1)
+
+    # If any stocks were owned
+    if (len(folio)>0):
+        # Print a value for every currency
+        folio_str = "    FOLIO VAL"
+        for currency,val in folio.items():
+            folio_str += "  $"+str(round(val,2))+" "+currency
+
+        scroll_print_float_str(folio_str, interval=0.25)
+        flp.clear()
+        flp.show()
+        time.sleep(1)
+
+
+# Four Letter pHAT helper
+def scroll_print_float_str(strn, interval=0.3):
+    if (len(strn)>0):
+        time.sleep(interval*3.0)
+
+        for ln in [strn[x:x+5] if ('.' in strn[x:x+5]) else strn[x:x+4] for x in xrange(0,len(strn)-3)]:
+            if (ln[0]!='.'):
+                flp.print_number_str(ln)
+                flp.show()
+                time.sleep(interval)
+
+        time.sleep(interval*2.0)
+
+# Configuration Loader
+def get_stocks_from_config(conf_filename):
+    if (len(sys.argv)>1):
+        conf_filename = sys.argv[1]
+
+    try:
+        with open(conf_filename) as conf_f:
+            lines = conf_f.readlines()
+            # Strip whitespace and capitalize, remove comments and empty lines
+            lines[:] = [x.strip().upper() for x in lines if x.strip()]
+            lines[:] = [x.split() for x in lines if not x.startswith("#")]
+
+            # Stocks list of ('symbol', shares, Share())
+            return [(x[0], float(x[1]), Share(x[0])) if len(x)>1 else (x[0],0.0,Share(x[0])) for x in lines]
+
+    except:
+        sys.exit("Error loading stock info from file: "+conf_filename)
+
+# System and error handler
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt, SystemExit:
+        print('Exiting...')
+        sys.exit(0)
